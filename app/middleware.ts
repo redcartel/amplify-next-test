6f959e2b295d7bd02d136692ddcbe4323649ab14
Carter Adams <carteradams@gmail.com>
2024-10-17 09:57:08 -0400

    middleware attempt 1

---
+++ app/middleware.ts
@@ -0,0 +1,19 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function middleware(req: NextRequest) {
+    console.log('ok');
+    return NextResponse.rewrite('/one');
+}
+
+export const config = {
+    matcher: [
+      /*
+       * Match all request paths except for the ones starting with:
+       * - api (API routes)
+       * - _next/static (static files)
+       * - _next/image (image optimization files)
+       * - favicon.ico (favicon file)
+       */
+      '/((?!api|_next/static|_next/image|favicon.ico).*)',
+    ],
+  }
---
+++ app/one/page.tsx
@@ -0,0 +1,3 @@
+export default function OnePage() {
+    return <div>One</div>
+}
